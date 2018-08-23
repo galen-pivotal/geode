2c4311c7e6ef7be03a2b8f5e3ad1c6cc65472fce
Galen O'Sullivan <gosullivan@pivotal.io>
2018-08-23 11:44:31 -0700

    GEODE-4650: remove caching of elder.
    This avoids an issue where in some cases, the cached value could be set
    to a member that has left the cluster, causing the DLockService to hang.
    
    Signed-off-by: Dan Smith <dsmith@pivotal.io>
    Signed-off-by: Galen O'Sullivan <gosullivan@pivotal.io>

--- geode-core/src/main/java/org/apache/geode/distributed/internal/locks/DLockService.java
+++ geode-core/src/main/java/org/apache/geode/distributed/internal/locks/DLockService.java
@@ -2800,7 +2800,8 @@ public class DLockService extends DistributedLockService {
    *
    * @param dm our local DM
    */
-  public static void recoverLocalElder(DistributionManager dm, Map grantors, Set needsRecovery) {
+  public static void recoverLocalElder(DistributionManager dm, Map<String, GrantorInfo> grantors,
+      Set<String> needsRecovery) {
     synchronized (services) {
       Iterator entries = services.entrySet().iterator();
       while (entries.hasNext()) {
@@ -2811,7 +2812,7 @@ public class DLockService extends DistributedLockService {
         DLockGrantor grantor = service.getGrantor();
         if (grantor != null && grantor.getVersionId() != -1 && !grantor.isDestroyed()) {
           foundGrantor = true;
-          GrantorInfo oldgi = (GrantorInfo) grantors.get(serviceName);
+          GrantorInfo oldgi = grantors.get(serviceName);
           if (oldgi == null || oldgi.getVersionId() < grantor.getVersionId()) {
             grantors.put(serviceName, new GrantorInfo(dm.getId(), grantor.getVersionId(),
                 service.getSerialNumber(), false));
