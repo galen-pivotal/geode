1a8671d34df3b69483b470620febc4aa61d96084
Dale Emery <demery@pivotal.io>
2018-12-21 11:50:42 -0800

    Document MicrometerRegistrar

--- geode-core/src/main/java/org/apache/geode/internal/statistics/micrometer/MicrometerRegistrar.java
+++ geode-core/src/main/java/org/apache/geode/internal/statistics/micrometer/MicrometerRegistrar.java
@@ -27,13 +27,43 @@ import org.apache.geode.StatisticDescriptor;
 import org.apache.geode.Statistics;
 import org.apache.geode.StatisticsType;
 
+/**
+ * Maintains a meter for each statistic value in each registered {@link Statistics} instance.
+ * <p>
+ * Each meter is given a name of the form <em>typeName.descriptorName</em>, where
+ * <em>typeName</em> is the name of the statistics instance's {@link StatisticsType}, and
+ * <em>descriptorName</em> is the name of the {@link StatisticDescriptor} that defines the
+ * statistic value.
+ * <p>
+ * Each meter is given a tag with key {@code "name"} and value <em>statisticsId</em>, where
+ * <em>statisticsId</em> is the statistics instance's {@code textID}.
+ * <p>
+ * Each meter's type is determined by the associated statistic descriptor's {@link
+ * StatisticDescriptor#isCounter() isCounter()} method. If {@code isCounter()} is true, the meter is
+ * a {@link FunctionCounter}. Otherwise the meter is a {@link Gauge}.
+ * <p>
+ * When sampled, each meter obtains its value extracting the statistics instance's value for the
+ * associated descriptor.
+ */
 public class MicrometerRegistrar {
   private final MeterRegistry registry;
 
+  /**
+   * Creates a registrar that maintains a meter in the given registry for each statistic value in
+   * each registered {@link Statistics} instance.
+   *
+   * @param registry the registry in which to maintain meters
+   */
   public MicrometerRegistrar(MeterRegistry registry) {
     this.registry = registry;
   }
 
+  /**
+   * Creates a meter for each statistic value defined by the given {@link Statistics} instance and
+   * adds each meter to the meter registry.
+   *
+   * @param statistics the statistics for which to create and register meters
+   */
   public void registerStatistics(Statistics statistics) {
     StatisticsType type = statistics.getType();
     for (StatisticDescriptor descriptor : type.getStatistics()) {
@@ -41,6 +71,11 @@ public class MicrometerRegistrar {
     }
   }
 
+  /**
+   * Removes the meters associated with the given statistics instance from the meter registry.
+   *
+   * @param statistics the statistics for which to deregister meters
+   */
   public void deregisterStatistics(Statistics statistics) {
     StatisticsType type = statistics.getType();
     Arrays.stream(type.getStatistics())
