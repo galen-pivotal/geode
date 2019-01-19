23f4c5df195b25552fd92d0cfea33239907d68f9
Dale Emery <demery@pivotal.io>
2019-01-18 16:50:49 -0800

    Add nascent integration test for composite registry

--- geode-core/src/test/java/org/apache/geode/internal/metrics/CompositeMeterManagerTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/metrics/CompositeMeterManagerTest.java
@@ -18,14 +18,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Set;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Measurement;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class CompositeMeterManagerTest {
   private final CompositeMeterRegistry primaryRegistry = new CompositeMeterRegistry();
-  private final CompositeMeterManager manager = new CompositeMeterManager(primaryRegistry);
+  private final CompositeMeterManager meterManager = new CompositeMeterManager(primaryRegistry);
 
   @Test
   public void remembersItsPrimaryRegistry() {
@@ -38,10 +41,10 @@ public class CompositeMeterManagerTest {
   }
 
   @Test
-  public void connectsDownstreamRegistry() {
+  public void addsDownstreamRegistry() {
     MeterRegistry downstream = new SimpleMeterRegistry();
 
-    manager.addDownstreamRegistry(downstream);
+    meterManager.addDownstreamRegistry(downstream);
 
     assertThat(primaryRegistry.getRegistries())
         .contains(downstream);
@@ -50,9 +53,9 @@ public class CompositeMeterManagerTest {
   @Test
   public void removesDownstreamRegistry() {
     MeterRegistry downstream = new SimpleMeterRegistry();
-    manager.addDownstreamRegistry(downstream);
+    meterManager.addDownstreamRegistry(downstream);
 
-    manager.removeDownstreamRegistry(downstream);
+    meterManager.removeDownstreamRegistry(downstream);
 
     assertThat(primaryRegistry.getRegistries())
         .doesNotContain(downstream);
@@ -71,4 +74,49 @@ public class CompositeMeterManagerTest {
     assertThat(downstreamRegistries)
         .isEmpty();
   }
+
+  @Ignore("pending investigation of Micrometer composite meter registry behavior")
+  @Test
+  public void connectsExistingMetersToNewDownstreamRegistries() {
+    MeterRegistry primaryRegistry = meterManager.getPrimaryRegistry();
+    meterManager.addDownstreamRegistry(new SimpleMeterRegistry());
+
+    Counter counter = primaryRegistry.counter("my.counter");
+    counter.increment(1.0);
+
+    System.out.println("counter = " + counter);
+    System.out
+        .println("counter.measure().iterator().next() = " + counter.measure().iterator().next());
+
+    MeterRegistry downstreamRegistry = new SimpleMeterRegistry();
+    meterManager.addDownstreamRegistry(downstreamRegistry);
+    double theAmountIncrementedAfterConnecting = 42.0;
+    counter.increment(theAmountIncrementedAfterConnecting);
+
+    Counter foundCounter = downstreamRegistry.find("my.counter").counter();
+    assertThat(foundCounter)
+        .isNotNull();
+    Measurement downstreamMeasurement = foundCounter.measure().iterator().next();
+    Measurement primaryMeasurement = counter.measure().iterator().next();
+
+    System.out.println(downstreamMeasurement.getValue());
+    assertThat(downstreamMeasurement.getValue())
+        .isEqualTo(primaryMeasurement.getValue());
+    System.out.println("primaryMeasurement = " + primaryMeasurement);
+    foundCounter.increment(2.0);
+    System.out.println(downstreamMeasurement.getValue());
+    assertThat(downstreamMeasurement.getValue())
+        .isEqualTo(primaryMeasurement.getValue());
+
+    // ensure that the downstream meter has the right tags
+    // maybe repeat (starting with updating the value)
+  }
+
+  @Ignore
+  @Test
+  public void connectsNewMetersToExistingDownstreamRegistries() {
+
+  }
+
+
 }
